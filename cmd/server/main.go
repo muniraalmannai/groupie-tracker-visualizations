e7b5cc540dc910551756a0e3b445928bd642f6ed
malmannai <muneeraalmannai3@gmail.com>
2024-11-13 13:24:15 +0300

    print link

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -120,6 +120,7 @@ func main() {
 
 	// Start the server
 	log.Println("Server is running on port 8080...")
+	log.Println("Server is accessible at: http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
